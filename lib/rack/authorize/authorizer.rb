aace096b10fe21f116c50d480b849e3518b3c3e7
Tommaso Patrizi <tommasop@gmail.com>
2015-10-01 17:24:53 +0200

    just debugging

--- lib/rack/authorize/authorizer.rb
+++ lib/rack/authorize/authorizer.rb
@@ -12,6 +12,10 @@ module Rack::Authorize
         method = env["REQUEST_METHOD"]
         path = env["PATH_INFO"]
         # The JWT payload is saved in rack.jwt.session the scopes key is scopes
+        puts "----------------------------"
+        puts env
+        puts env.fetch("rack.jwt.session", {})[:scopes] 
+        puts "----------------------------"
         scopes = env.fetch("rack.jwt.session", {})[:scopes] 
         return [403, {}, ["Access Forbidden"]] unless @block.call(method, path, scopes)
       end
