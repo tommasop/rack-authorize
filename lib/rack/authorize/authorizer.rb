8c0648de1d22b84ba50555f836aa29366e6a6d31
Tommaso Patrizi <tommasop@gmail.com>
2015-10-01 10:16:05 +0200

    adding scopes for jwt ability matching

--- lib/rack/authorize/authorizer.rb
+++ lib/rack/authorize/authorizer.rb
@@ -1,14 +1,15 @@
 module Rack::Authorize
   class Authorizer
-    def initialize(app, &block)
+    def initialize(app, scopes, &block)
       @app = app
+      @scopes = scopes
       @block = block
     end
 
     def call(env)
       method = env["REQUEST_METHOD"]
       path = env["PATH_INFO"]
-      return [403, {}, ["Access Forbidden"]] unless @block.call(method, path)
+      return [403, {}, ["Access Forbidden"]] unless @block.call(method, path, @scopes)
       @app.call(env)
     end
   end
