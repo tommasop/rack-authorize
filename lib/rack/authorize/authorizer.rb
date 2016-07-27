339c54ae30beebf979cdbaec8a1573faf0308259
Tommaso Patrizi <tpatrizi@makeplan.it>
2016-07-27 09:29:18 +0200

    fixed case jwt session is a string

--- lib/rack/authorize/authorizer.rb
+++ lib/rack/authorize/authorizer.rb
@@ -16,9 +16,13 @@ module Rack::Authorize
         puts "----------------------------"
         puts env
         puts "----------------------------"
-        puts @auth_definition
-        puts env.fetch("rack.jwt.session", {}).class
-        scopes = Oj.load(env.fetch("rack.jwt.session", {})[@auth_definition])
+        jwt_session_data = env.fetch("rack.jwt.session", {})
+        if jwt_session_data.is_a? String
+          jwt_session_data = Oj.load(jwt_session_data)
+          scopes = jwt_session_data.fetch(@auth_definition.to_sym, {})
+        else
+          scopes = Oj.load(env.fetch("rack.jwt.session", {})[@auth_definition])
+        end
         return [403, {}, ["Access Forbidden"]] unless @block.call(method, path, scopes)
       end
       @app.call(env)
